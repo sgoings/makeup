d269ec9ba60b4f0f42c217f71d2cc3bce7ebc55b
Seth Goings <sgoings@engineyard.com>
2015-12-15 16:05:42 -0700

    add basic command structure (add, status, init)

--- conventions/deis/main.mk
+++
@@ -1,9 +0,0 @@
-include $(MAKEUP_DIR)/info/main.mk \
-				$(MAKEUP_DIR)/docker/main.mk \
-				$(MAKEUP_DIR)/split-org-registry/main.mk
-
-.PHONY: build
-build: docker-build
-
-.PHONY: push
-push: docker-push
